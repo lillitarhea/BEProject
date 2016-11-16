9719d7bffd5adcedc83a76b23cc47db583fa4b14
lillitarhea <lillitarhea@gmail.com>
2016-11-16 19:38:39 +0530

    First UI has been added

---
+++ KeydroidApplication/app/src/main/java/com/example/android/keydroidapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.keydroidapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, Main2Activity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+
+    }
+}
